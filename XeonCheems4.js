417dd5b3b9db6370c05e1aad57e57828696d63df
HexZaor <108601619+HexZaor@users.noreply.github.com>
2022-07-07 12:05:13 +0800

    Update XeonCheems4.js

--- XeonCheems4.js
+++ XeonCheems4.js
@@ -8766,8 +8766,8 @@ title:"I deserve something for my hardwork",
 body: "Click to donate", 
 thumbnail: fs.readFileSync("XeonMedia/theme/cheemspic.jpg"),
 mediaType:1,
-mediaUrl: 'https://telegra.ph/file/8737b098fd5702daeb7e0.jpg',
-sourceUrl: "https://telegra.ph/file/8737b098fd5702daeb7e0.jpg"
+mediaUrl: 'https://telegra.ph/file/ae2496b6f6c4f475f4845.jpg',
+sourceUrl: "https://telegra.ph/file/ae2496b6f6c4f475f4845.jpg"
 }}
 }
 XeonBotInc.sendMessage(m.chat, buttonMessage, { quoted: m })
